b8a6607f204c8321567dbe415c96588e707b6d07
adard2002 <adard2002@gmail.com>
2021-03-02 11:31:42 -0600

    fixed it so it at least shows an error when you load nodemon:

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 `use strict`;
 
-
 //Load Environment Variables from the .env file
 require('dotenv').config();
 
@@ -9,7 +8,7 @@ const express = require('express');
 // const cors = require('cors');
 const pg = require('pg');
 pg.defaults.ssl = process.env.NODE_ENV === 'production' && { rejectUnauthorized: false };
-const superagent = require('superagent'); 
+const superagent = require('superagent');
 const methodOverride = require('method-override');
 
 // Database Setup
