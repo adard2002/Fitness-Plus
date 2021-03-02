a582e11d9f61baeb1e141abd633b5b23d7e42030
adard2002 <adard2002@gmail.com>
2021-03-01 18:04:59 -0600

    adding fixes to the server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-`use strict`
+`use strict`;
 
 // TO DO:
 var slideIndex = 0;
@@ -6,12 +6,12 @@ showSlides();
 
 function showSlides() {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName('mySlides');
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = 'none';
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
+  if (slideIndex > slides.length) {slideIndex = 1;}
+  slides[slideIndex-1].style.display = 'block';
   setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
