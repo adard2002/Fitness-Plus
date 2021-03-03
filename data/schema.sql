5272a8d83c93114a7fd7c42a3f4235467fd81dc6
Vavyo <vavyo2121@gmail.com>
2021-03-03 16:42:32 -0600

    database schema updated with 3 tables

--- data/schema.sql
+++ data/schema.sql
@@ -1,16 +1,36 @@
-CREATE TABLE WorkoutTable (
-    Id SERIAL PRIMARY KEY,
-    workout_date DATE NOT NULL,
-    week_day VARCHAR(255) NOT NULL,
-    exercise VARCHAR(255) NOT NULL,
-    focus_area VARCHAR(255),
-    liift_round INT,
-    weight_used DECIMAL(10,2) NOT NULL
+DROP TABLE IF EXISTS userWorkout;
+DROP TABLE IF EXISTS exercises;
+DROP TABLE IF EXISTS username;
+
+CREATE TABLE exercises (
+    exercise_id SERIAL PRIMARY KEY,
+    exercise_name VARCHAR(255) NOT NULL,
+    category VARCHAR(255) NOT NULL
 );
 
---CREATE TABLE Liift4 (
-   -- Id SERIAL PRIMARY Key,
-   -- WeekAndDay VARCHAR(255),
-   -- Exercise VARCHAR(255),
-  --  FocusArea VARCHAR(255)
---);
+
+
+CREATE TABLE username (
+  username VARCHAR(255) PRIMARY KEY
+);
+
+
+
+CREATE TABLE userWorkout (
+  id SERIAL PRIMARY KEY
+  , username VARCHAR(255) REFERENCES username (username)
+  , workout_id INTEGER REFERENCES exercises (exercise_id)
+--  , startDate DATE NOT NULL
+--  , weight_used DECIMAL(10,2)
+  , workout_desc VARCHAR(255)
+  , equipment VARCHAR(255)
+);
+
+-- --  seed info
+-- INSERT INTO username (username) VALUES('Nathan');
+
+-- INSERT INTO exercises (exercise_name, category)
+--   VALUES('deadlift', '13');
+
+-- INSERT INTO userWorkout (username, workout_id, workout_desc, equipment)
+--   VALUES('Nathan', 1, 'test workout', 'dumbybell');
